5d424c277eec6d3197dc4818bd19662181a6c1cb
tharun <madoththarun22@gamail.com>
2023-10-11 15:21:15 +0000

    solution

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,25 @@
+// Write your code here.
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {thumbnailDetails, updateThumbnail, isActive} = props
+  const {id, thumbnailUrl, thumbnailAltText} = thumbnailDetails
+  const thumbnailClassName = isActive ? 'thumbnail active' : 'thumbnail'
+  const onClickImage = () => {
+    updateThumbnail(id)
+  }
+
+  return (
+    <li className="thumbnail-list-item">
+      <button type="button" className="thumbnail-button" onClick={onClickImage}>
+        <img
+          className={thumbnailClassName}
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
